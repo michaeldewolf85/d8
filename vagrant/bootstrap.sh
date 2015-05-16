2b765abf5c9e5d0e3cca0ada1fd7cc1014f2722c
Michael DeWolf <michaeldewolf85@gmail.com>
2015-05-16 18:12:27 -0400

    Improve formatting on bootstrap script.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,9 +4,24 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
+
+##########
+# Apache #
+##########
 apt-get install apache2 -y
-# Add a servername to httpd.conf in order to avoid Apache warning.
 echo "ServerName localhost" | sudo tee /etc/apache2/httpd.conf
 
-apt-get install -y php5 libapache2-mod-php5 mysql-server php5-mysql
+#########
+# MySQL #
+#########
+apt-get install -y mysql-server
+
+#######
+# PHP #
+#######
+apt-get install -y php5 libapache2-mod-php5 php5-mysql
+
+################
+# Virtual Host #
+################
 
