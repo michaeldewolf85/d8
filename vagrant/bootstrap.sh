3bea5cf66f209114ebcb050cc82a78836091a4b0
Michael DeWolf <michaeldewolf85@gmail.com>
2015-05-17 12:09:40 -0400

    Configure default and SSL vhosts in Apache during vagrant bootstrap.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -10,6 +10,7 @@ apt-get update
 ##########
 apt-get install apache2 -y
 echo "ServerName localhost" | sudo tee /etc/apache2/httpd.conf
+a2enmod ssl
 
 #########
 # MySQL #
@@ -24,4 +25,10 @@ apt-get install -y php5 libapache2-mod-php5 php5-mysql
 ################
 # Virtual Host #
 ################
+cp /etc/apache2/sites-available/default /etc/apache2/sites-available/d8
+cp /etc/apache2/sites-available/default-ssl /etc/apache2/sites-available/d8-ssl
+sed -i '/ServerAdmin webmaster@localhost/a\\tServerName sbx.d8.dev' /etc/apache2/sites-available/d8*
+sed -i 's/\/var\/www/\/vagrant\/docroot/g' /etc/apache2/sites-available/d8*
+a2ensite d8 d8-ssl
+service apache2 restart
 
